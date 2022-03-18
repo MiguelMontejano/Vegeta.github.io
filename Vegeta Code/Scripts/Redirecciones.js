ec5b84215cfbe5720fac8cb03b6d33d32693a703
Miguel Montejano <miguelmontejanoprofesional@gmail.com>
2022-03-18 01:21:40 +0100

    Solución del error que permite empezar una partida con solo un jugador

--- Vegeta Code/Scripts/Redirecciones.js
+++ Vegeta Code/Scripts/Redirecciones.js
@@ -1,5 +1,12 @@
 const redirigirHome = () =>{
-    window.location.href = "Home.html";
+    arrayJugadores = JSON.parse(localStorage.getItem('partida'));
+
+    if(arrayJugadores.length < 2){
+        alert('El número mínimo de jugadores son 2. Por favor añade un jugador más.')
+    } else {
+        window.location.href = "Home.html";
+    }
+
 }
 
 const redirigirSetup = () =>{
