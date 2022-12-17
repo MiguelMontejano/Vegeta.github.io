2661676475e11b8701f862887862920bb61c13d3
Miguel Montejano <miguelmontejanoprofesional@gmail.com>
2022-12-17 18:21:25 +0100

    update redirectin

--- Scripts/Redirecciones.js
+++ Scripts/Redirecciones.js
@@ -13,7 +13,7 @@ const redirigirSetup = () =>{
     var sPath = window.location.pathname;
     var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
     if(sPage == "index.html"){
-        window.location.href = "Html/Setup.html";
+        window.location.href = "./Html/Setup.html";
     }
     else{
         window.location.href = "Setup.html";
